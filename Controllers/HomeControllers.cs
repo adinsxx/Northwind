7bd38ae373110aaa4338921c5f253bb89817166d
jornelas4 <jornelas4@my.wctc.edu>
2021-10-05 01:26:06 -0500

    Added Discount Controller

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    
+    {
+        private NorthwindContext _northwindContext;
+        public HomeController(NorthwindContext db) => _northwindContext = db;
+
+        public ActionResult Index() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
+    }
+}
